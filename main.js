4f4ca2c952e28921b10c29d995ed616780fd421a
JPM-Boy <54916535+JPM-Boy@users.noreply.github.com>
2019-09-05 00:06:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var gameData = {
+  gold: 0,
+  goldPerClick: 1,
+  goldPerClickCost: 10
+}
+
+function mineGold() {
+  gameData.gold += gameData.goldPerClick
+  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+}
+
+function buyGoldPerClick() {
+  if (gameData.gold >= gameData.goldPerClickCost) {
+    gameData.gold -= gameData.goldPerClickCost
+    gameData.goldPerClick += 1
+    gameData.goldPerClickCost *= 2
+    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+  }
+}
+var saveGameLoop = window.setInterval(function() {
+  localStorage.setItem('goldMinerSave', JSON.stringify(gameData))
+}, 15000)
+var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
+if (savegame !== null) {
+  gameData = savegame
+}
