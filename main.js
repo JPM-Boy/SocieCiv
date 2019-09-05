67c2f7e2060b2b82b3bc4e4cb99604957c4b1a43
JPM-Boy <54916535+JPM-Boy@users.noreply.github.com>
2019-09-05 13:11:04 +0200

    Rock Update, Auto-Rocks

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var RockData = {
   rocks: 0,
   rockPerClick: 1,
-  rockPerClickCost: 50
+  rockPerClickCost: 50,
+  rockPerSecond: 0,
+  rockPerSecondCost: 100
 }
 
 function recollectRock() {
   RockData.rocks += RockData.rockPerClick
+   document.getElementById("RocksRecollected").innerHTML = RockData.rocks + " Rocks Recollected"
+}
+
+function SecondRock() {
+  RockData.rocks += RockData.rockPerSecond
    document.getElementById("RocksRecollected").innerHTML = RockData.rocks + " Rocks Recollected"
 }
 
@@ -18,3 +25,17 @@ function buyRockPerClick() {
     document.getElementById("perClickRockUpgrade").innerHTML = "Send People to Recollect Rocks (" + RockData.rockPerClick + ") (" + RockData.rockPerClickCost + ")"
   }
 }
+
+function buyRockPerSecond() {
+  if (RockData.rocks >= RockData.rockPerSecondCost) {
+    RockData.rocks -= RockData.rockPerSecondCost
+    RockData.rockPerSecond += 1
+    RockData.rockPerSecondCost *= 2
+    document.getElementById("RocksRecollected").innerHTML = RockData.rocks + " Rocks Recollected"
+    document.getElementById("perSecondRockUpgrade").innerHTML = "Rock Field (" + RockData.rockPerSecond + ") (" + RockData.rockPerSecondCost + ")"
+  }
+}
+
+var GameLoop = window.setInterval(function() {
+  SecondRock()
+}, 1000)
