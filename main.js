1f4425618551424208cd6d4b93bc66b5c0a3934d
JPM-Boy <54916535+JPM-Boy@users.noreply.github.com>
2019-09-06 16:23:13 +0200

    AUTOSAVE AND RESET Button added.

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
-var RockData = {
+var GameData = {
   rocks: 0,
   rockPerClick: 1,
   rockPerClickCost: 50,
   rockPerSecond: 0,
-  rockPerSecondCost: 100
+  rockPerSecondCost: 100,
+  humans: 1,
+  humansBusy: 0,
+  humansFree: 0,
 }
 
 function recollectRock() {
-  RockData.rocks += RockData.rockPerClick
-   document.getElementById("RocksRecollected").innerHTML = RockData.rocks + " Rocks Recollected"
+  GameData.rocks += GameData.rockPerClick
+   document.getElementById("RocksRecollected").innerHTML = GameData.rocks + " Rocks Recollected"
 }
 
 function SecondRock() {
-  RockData.rocks += RockData.rockPerSecond
-   document.getElementById("RocksRecollected").innerHTML = RockData.rocks + " Rocks Recollected"
+  GameData.rocks += GameData.rockPerSecond
+   document.getElementById("RocksRecollected").innerHTML = GameData.rocks + " Rocks Recollected"
 }
 
 function buyRockPerClick() {
-  if (RockData.rocks >= RockData.rockPerClickCost) {
-    RockData.rocks -= RockData.rockPerClickCost
-    RockData.rockPerClick += 1
-    RockData.rockPerClickCost *= 2
-    document.getElementById("RocksRecollected").innerHTML = RockData.rocks + " Rocks Recollected"
-    document.getElementById("perClickRockUpgrade").innerHTML = "Send People to Recollect Rocks (" + RockData.rockPerClick + ") (" + RockData.rockPerClickCost + ")"
+  if (GameData.rocks >= GameData.rockPerClickCost) {
+    GameData.rocks -= GameData.rockPerClickCost
+    GameData.rockPerClick += 1
+    GameData.rockPerClickCost *= 2
+    document.getElementById("RocksRecollected").innerHTML = GameData.rocks + " Rocks Recollected"
+    document.getElementById("perClickRockUpgrade").innerHTML = "Send People to Recollect Rocks (" + GameData.rockPerClick + ") (" + GameData.rockPerClickCost + ")"
   }
 }
 
 function buyRockPerSecond() {
-  if (RockData.rocks >= RockData.rockPerSecondCost) {
-    RockData.rocks -= RockData.rockPerSecondCost
-    RockData.rockPerSecond += 1
-    RockData.rockPerSecondCost *= 2
-    document.getElementById("RocksRecollected").innerHTML = RockData.rocks + " Rocks Recollected"
-    document.getElementById("perSecondRockUpgrade").innerHTML = "Rock Field (" + RockData.rockPerSecond + ") (" + RockData.rockPerSecondCost + ")"
+  if (GameData.rocks >= GameData.rockPerSecondCost) {
+    GameData.rocks -= GameData.rockPerSecondCost
+    GameData.rockPerSecond += 1
+    GameData.rockPerSecondCost *= 2
+    document.getElementById("RocksRecollected").innerHTML = GameData.rocks + " Rocks Recollected"
+    document.getElementById("perSecondRockUpgrade").innerHTML = "Rock Field (" + GameData.rockPerSecond + ") (" + GameData.rockPerSecondCost + ")"
   }
 }
 
@@ -44,6 +47,31 @@ function tab(tab) {
 }
 tab("RockMenu")
 
+function Refresh() {
+  document.getElementById("RocksRecollected").innerHTML = GameData.rocks + " Rocks Recollected"
+  document.getElementById("perSecondRockUpgrade").innerHTML = "Rock Field (" + GameData.rockPerSecond + ") (" + GameData.rockPerSecondCost + ")"
+  document.getElementById("perClickRockUpgrade").innerHTML = "Send People to Recollect Rocks (" + GameData.rockPerClick + ") (" + GameData.rockPerClickCost + ")"
+}
+
 var GameLoop = window.setInterval(function() {
   SecondRock()
+  Refresh()
 }, 1000)
+
+var SaveGame = window.setInterval(function() {
+  localStorage.setItem("GlobalSave", JSON.stringify(GameData))
+}, 15000)
+
+var SaveGame = JSON.parse(localStorage.getItem("GlobalSave"))
+if (SaveGame !== null) {
+  GameData = SaveGame
+}
+
+function reset() {
+  localStorage.removeItem("GlobalSave")
+  GameData.rocks = 0
+  GameData.rockPerClick = 1
+  GameData.rockPerClickCost= 50
+  GameData.rockPerSecond = 0
+  GameData.rockPerSecondCost = 100
+}
